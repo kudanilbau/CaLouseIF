82c6f2efe723a14023d332fc66cbf662cc10e19a
Calvin Farrellino Kurniawan <calvinfarrellinok@gmail.com>
2024-12-04 05:09:37 +0700

    remove upload file

--- src/view/SellerEditView.java
+++ src/view/SellerEditView.java
@@ -1,20 +1,15 @@
 package view;
 
-import java.io.File;
-
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
 
 public class SellerEditView {
 	private VBox mainVBox;
-	private TextField namaItem, categoryItem, ukuranItem, hargaItem;
-	private Button uploadButton, submitButton;
-	private FileChooser pilihFile;
+	private TextField idItem, namaItem, categoryItem, ukuranItem, hargaItem;
+	private Button submitButton;
 
 	public SellerEditView() {
 		initComponent();
@@ -25,46 +20,31 @@ public class SellerEditView {
 
 	public void initComponent() {
 		mainVBox = new VBox(15);
+		idItem = new TextField();
 		namaItem = new TextField();
 		categoryItem = new TextField();
 		ukuranItem = new TextField();
 		hargaItem = new TextField();
 
+		idItem.setPromptText("Id item");
 		namaItem.setPromptText("Masukkan nama item");
 		categoryItem.setPromptText("Masukkan kategori item");
 		ukuranItem.setPromptText("Masukkan ukuran item");
 		hargaItem.setPromptText("Masukkan harga item");
 
-		uploadButton = new Button("Upload Image");
 		submitButton = new Button("Submit");
-
-		pilihFile = new FileChooser();
 	}
 
 	public void addComponent() {
-		mainVBox.getChildren().addAll(namaItem, categoryItem, ukuranItem, hargaItem, uploadButton, submitButton);
+		mainVBox.getChildren().addAll(idItem, namaItem, categoryItem, ukuranItem, hargaItem, submitButton);
 	}
 
 	public void styleComponent() {
 		mainVBox.setPadding(new Insets(10));
+		idItem.setDisable(true);
 	}
 
 	public void initActionComponent() {
-		uploadButton.setOnAction(e -> {
-			Stage primaryStage = (Stage) ((Node) e.getSource()).getScene().getWindow();
-			pilihFile.setTitle("Pilih gambar yang ingin di Upload");
-
-			pilihFile.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Image files", "*.jpg"),
-					new FileChooser.ExtensionFilter("Image files", "*.png"));
-			File fileTerpilih = pilihFile.showOpenDialog(primaryStage);
-			if (fileTerpilih != null) {
-				String filepath = fileTerpilih.getAbsolutePath();
-				System.out.println("File Path : " + filepath);
-			} else {
-				System.err.println("No File Selected");
-			}
-		});
-
 		submitButton.setOnAction(e -> {
 
 		});
