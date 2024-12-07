0822f016853438bfb643da99e3cb73737307fdab
Grinder7 <calvinfarrellinok@gmail.com>
2024-12-07 17:18:25 +0700

    remove action bar

--- src/view/BuyerHomepageView.java
+++ src/view/BuyerHomepageView.java
@@ -5,26 +5,20 @@ import java.util.Arrays;
 import controller.BuyerHomepageController;
 import controller.ItemController;
 import javafx.collections.ObservableList;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseButton;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import model.Item;
 
 public class BuyerHomepageView extends BorderPane {
 	private TableView<Item> itemTable;
-	private Button purchaseButton, offerButton, addToWishlistButton;
 	private ObservableList<Item> itemList;
 	private TableColumn<Item, String> nameColumn, categoryColumn, sizeColumn, priceColumn;
 	private VBox mainContent;
-	private HBox actionBar;
 
 	private ItemController itemController;
 	private BuyerHomepageController buyerHomepageController;
@@ -56,23 +50,12 @@ public class BuyerHomepageView extends BorderPane {
 		itemTable.setItems(itemList);
 
 		mainContent = new VBox();
-		actionBar = new HBox(10);
-
-		purchaseButton = new Button("Purchase");
-		offerButton = new Button("Offer");
-		addToWishlistButton = new Button ("Add To Wishlist");
 
 	}
 
 	private void addComponents() {
 		mainContent.getChildren().add(itemTable);
-
-		actionBar.getChildren().addAll(purchaseButton, offerButton, addToWishlistButton);
-		actionBar.setAlignment(Pos.CENTER);
-		actionBar.setPadding(new Insets(10));
-
 		this.setCenter(mainContent);
-		this.setBottom(actionBar);
 	}
 
 	private void styleComponents() {
