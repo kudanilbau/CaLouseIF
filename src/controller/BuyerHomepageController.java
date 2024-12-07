d413c634468797eee1f83c39391263fbff93d89a
Grinder7 <calvinfarrellinok@gmail.com>
2024-12-07 17:10:16 +0700

    move function

--- src/controller/BuyerHomepageController.java
+++ src/controller/BuyerHomepageController.java
@@ -1,18 +1,6 @@
 package controller;
 
 import model.Item;
-import router.Router;
 
 public class BuyerHomepageController {	
-	public void handlePurchaseButton(Item item) {
-		
-	}
-	
-	public void handleOfferButton(Item item) {
-		
-	}
-	
-	public void handleAddToWishlist(Item item) {
-		
-	}
 }
---
+++ src/controller/BuyerItemDetailsController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import model.Item;
+
+public class BuyerItemDetailsController {
+	public void handlePurchaseButton(Item item) {
+		
+	}
+	
+	public void handleOfferButton(Item item) {
+		
+	}
+	
+	public void handleAddToWishlist(Item item) {
+		
+	}
+}
