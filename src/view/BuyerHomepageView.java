4eddc28ad7aaf1a7999030f580cee1b763e4b8eb
Grinder7 <calvinfarrellinok@gmail.com>
2024-12-07 15:58:36 +0700

    add purchase and offer button

--- src/view/BuyerHomepageView.java
+++ src/view/BuyerHomepageView.java
@@ -5,12 +5,14 @@ import java.util.Arrays;
 import controller.ItemController;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseButton;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import model.Item;
@@ -18,8 +20,11 @@ import router.Router;
 
 public class BuyerHomepageView extends BorderPane {
 	private TableView<Item> itemTable;
-	private Button wishlistButton, purchaseHistoryButton, homepageButton;
+	private Button wishlistButton, purchaseHistoryButton, homepageButton, purchaseButton, offerButton;
 	private ObservableList<Item> itemList;
+	private TableColumn<Item, String> nameColumn, categoryColumn, sizeColumn, priceColumn;
+	private VBox mainContent;
+	private HBox actionBar;
 
 	private ItemController itemController;
 
@@ -36,33 +41,51 @@ public class BuyerHomepageView extends BorderPane {
 
 	private void initComponents() {
 		itemTable = new TableView<>();
-		TableColumn<Item, String> nameColumn = new TableColumn<>("Item Name");
-		TableColumn<Item, String> categoryColumn = new TableColumn<>("Category");
-		TableColumn<Item, String> sizeColumn = new TableColumn<>("Size");
-		TableColumn<Item, Double> priceColumn = new TableColumn<>("Price");
+		nameColumn = new TableColumn<>("Item Name");
+		categoryColumn = new TableColumn<>("Category");
+		sizeColumn = new TableColumn<>("Size");
+		priceColumn = new TableColumn<>("Price");
+		
 		nameColumn.setCellValueFactory(new PropertyValueFactory<>("item_name"));
 		categoryColumn.setCellValueFactory(new PropertyValueFactory<>("item_category"));
 		sizeColumn.setCellValueFactory(new PropertyValueFactory<>("item_size"));
 		priceColumn.setCellValueFactory(new PropertyValueFactory<>("item_price"));
+		
 		itemTable.getColumns().addAll(Arrays.asList(nameColumn, categoryColumn, sizeColumn, priceColumn));
 		itemTable.setItems(itemList);
 
 		wishlistButton = new Button("Wishlist");
 		purchaseHistoryButton = new Button("Purchase History");
 		homepageButton = new Button("Homepage");
+		
+		mainContent = new VBox();
+		actionBar = new HBox(10);
+		
+		purchaseButton = new Button("Purchase");
+		offerButton = new Button("Offer");
+		
 	}
 
 	private void addComponents() {
-
-		VBox mainContent = new VBox(10, itemTable);
-		mainContent.setPadding(new Insets(10));
-
+		mainContent.getChildren().add(itemTable);
+		
+		actionBar.getChildren().addAll(purchaseButton, offerButton);
+		actionBar.setAlignment(Pos.CENTER);
+		actionBar.setPadding(new Insets(10));
+		
 		this.setCenter(mainContent);
+		this.setBottom(actionBar);
 	}
 
 	private void styleComponents() {
 		this.setPrefSize(Double.MAX_VALUE, Double.MAX_VALUE);
 		VBox.setVgrow(itemTable, Priority.ALWAYS);
+
+		itemTable.setColumnResizePolicy(TableView.UNCONSTRAINED_RESIZE_POLICY);
+		nameColumn.prefWidthProperty().bind(itemTable.widthProperty().multiply(0.4));
+		categoryColumn.prefWidthProperty().bind(itemTable.widthProperty().multiply(0.387));
+		sizeColumn.prefWidthProperty().bind(itemTable.widthProperty().multiply(0.1));
+		priceColumn.prefWidthProperty().bind(itemTable.widthProperty().multiply(0.1));
 	}
 
 	private void setActionNode() {
@@ -79,6 +102,18 @@ public class BuyerHomepageView extends BorderPane {
 		});
 
 		homepageButton.setOnMouseClicked(e -> {
+			if (e.getButton() == MouseButton.PRIMARY) {
+				
+			}
+		});
+		
+		purchaseButton.setOnMouseClicked(e -> {
+			if (e.getButton() == MouseButton.PRIMARY) {
+				
+			}
+		});
+		
+		offerButton.setOnMouseClicked(e -> {
 			if (e.getButton() == MouseButton.PRIMARY) {
 				
 			}
